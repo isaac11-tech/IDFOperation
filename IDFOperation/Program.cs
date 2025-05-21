27dbbbc9e5b9e75976894d722692ccb8746f2f22
isaac tunik <tunik.isaac@gmail.com>
2025-05-21 09:52:30 +0300

    vo1

---
+++ IDFOperation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IDFOperation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+                       StrikeOptions strike = new StrikeOptions();
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ IDFOperation/strikeOptions/F16.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFOperation.strikeOptions
+{
+    internal class F16
+    {
+    }
+}

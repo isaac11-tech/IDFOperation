4ef29b87cd2b8a0b390aac821916c562e8afd408
ezriel <ezrirot@gmail.com>
2025-05-22 12:55:35 +0300

    red zone -include classes: hammas terrorist abs_weapon weapons

---
+++ ConsoleApp1/NewFolder/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ConsoleApp1.AbstractClass;
+
+
+namespace ConsoleApp1.Enemy
+{
+    public class Terrorist
+    {
+        private string Name { get; }
+        private int Rank { get; set; }
+        private string Organizational_Afilliation { get; set; }
+        private char Status { get; set; }
+        private List<Weapon> Weapons { get; } = new List<Weapon>();
+
+
+        public Terrorist(string name, int rank, string affiliation, char status)
+        {
+            this.Name = name;
+            this.Rank = rank;
+            this.Organizational_Afilliation = affiliation;
+            this.Status = status;
+        }
+        public void AddWeapon(Weapon x)
+        {
+            Weapons.Add(x);
+        }
+        public void RemoveWeapon(Weapon x)
+        {
+            Weapons.Remove(x);
+        }
+
+
+
+
+
+    }
+}
